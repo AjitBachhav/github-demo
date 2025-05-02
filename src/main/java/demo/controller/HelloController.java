d10ba50ddebfa32b14410182be65cd9c35251ec9
Ajit Buvaji Bachhav <ajitbachhav1998@gmail.com>
2025-05-02 15:16:59 +0530

    final

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring Boot is running!";
+        return "Hello, Spring Boot is running!lets goo....................";
     }
 }
